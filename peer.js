2572d753abc8771c91241c34053485ca705a2a0f
Louis Sobel <sobel@mit.edu>
2014-04-22 12:31:40 -0400

    peer uses Server object

--- peer.js
+++ peer.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var zerorpc = require('zerorpc');
+var zerorpc = require('zerorpc')
+  , Server = require('./server').Server
+  ;
 
 var Peer = module.exports.Peer = function (name, port, masterport) {
   this.name = name;
   this.port = port;
-  this.masterport = masterport;
+  this.master = new Server('tcp://0.0.0.0:' + masterport, 'master');
 
   this._setupRpcServer();
 };
@@ -13,16 +15,15 @@ var Peer = module.exports.Peer = function (name, port, masterport) {
 Peer.prototype.start = function () {
   this._server.bind('tcp://0.0.0.0:' + this.port);
   console.log('Peer ' + this.name + ' started on ' + this.port);
-
-  this.master = new zerorpc.Client();
-  this.master.connect('tcp://0.0.0.0:' + this.masterport);
 };
 
 Peer.prototype.handleStream = function (filename, startingat, reply) {
   startingat = parseInt(startingat, 10);
-  var current = startingat;
+  var current = startingat
+    , client = this.master.getClient()
+    ;
   var _getOne = function () {
-    this.master.invoke('get', filename, current, function (err, res, more){
+    client.invoke('get', filename, current, function (err, res, more){
       reply(null, res, true);
       current++;
       _getOne();
